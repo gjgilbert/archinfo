295c2465a21b0da0f9e4515f9a87de1720acbb42
Greg Gilbert <gjgilbert@uchicago.edu>
2019-12-10 14:43:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.0']
+        'Programming Language :: Python :: 3']
 )
